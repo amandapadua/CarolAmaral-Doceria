663a53965882918366e62dd23e6dde10f053b14b
devpadua <amanda.p.transporteelogistica@gmail.com>
2026-06-17 13:10:45 -0300

    Atualiza produtos da doceria

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import './styles/global.css';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
-import ComoFunciona from './components/ComoFunciona';
 import Sobre from './components/Sobre';
 import Cardapio from './components/Cardapio';
 import FormularioEncomenda from './components/FormularioEncomenda';
@@ -14,7 +13,6 @@ export default function App() {
       <Navbar />
       <main>
         <Hero />
-        <ComoFunciona />
         <Sobre />
         <Cardapio />
         <FormularioEncomenda />
